1e2efeaf922661b29cc8121c1dd3f9c6beb0cac3
Lu <chris.lu@syccuxas01.pcc.edu>
2020-01-21 18:08:07 -0800

    branching and using 162 methodlogy

--- main.cpp
+++ main.cpp
@@ -5,51 +5,70 @@ using namespace std;
 
 int main() 
 {
-string userInput, ugh, inputName ;
-ifstream inData;
-txtRecord textRecord;
+  string userInput, ugh, inputName ;
+  ifstream inData;
+  txtRecord textRecord;
 
-cout << "name a file: ";
-cin >> textRecord.userInput;
-cout << "file is " << textRecord.userInput << endl;
+  //cout << "name a file: ";
+  //cin >> textRecord.userInput;
+  textRecord.userInput = "short.txt"; //uncomment above two lines+delete this one before production
 
-inputName = textRecord.userInput.c_str();
+  cout << "file is " << textRecord.userInput << endl;
 
-inData.open(inputName);
+  inputName = textRecord.userInput.c_str();
 
-char c;
-int address = 0;
-int lines = 0;
-while(inData.get(c)) //using a while loop bc we don't know how long the text will be
-{
-  textRecord.contents[address] = c; //putting it in by char to account for spaces
-  address++;  
-}
+  inData.open(inputName);
 
-inData.close(); //is there a way to cleanly run multiple loops? this is the only way I know to flush this buffer
-inData.open(inputName);
+  char c;
+  int address = 0;
+  int lines = 0;
+  while(inData.get(c)) //using a while loop bc we don't know how long the text will be
+  {
+    textRecord.contents[address] = c; //putting it in by char to account for spaces
+    address++;  
+  }
 
-while(!inData.eof())
-{
-if(getline(inData, ugh )) //this is our linecounting logic, still need to situate it
-{
-++lines;
-cout << ugh << "*" << endl; //how do you want to parse for last char
-}
+  inData.close(); //is there a way to cleanly run multiple loops? this is the only way I know to flush this buffer
+  inData.open(inputName);
 
+  while(!inData.eof())
+  {
+    if(getline(inData, ugh )) //this is our linecounting logic, still need to situate it
+    {
+      ++lines;
+      //  cout << ugh << "*" << endl; //how do you want to parse for last char
+    }
+  };
+  inData.close();
 
-};
+  inData.open(inputName);
+  //parse for words!!
 
-cout << "number of lines is " << lines << endl;
-cout << "total chars is " << address << endl;
-cout << "first line is x chars long " << endl;
+  int wholeLen = static_cast< int >(strlen(textRecord.contents)); //we get awful sign/unsign ereror withou static cast 
+  for(int i = 0; i < wholeLen; i++) //a single traversal through the text should work
+  {
+    int wordCountIndex = 0; //figured we could have it in this scope because it's likely not used anywhere else
+    if(textRecord.contents[i] != 32) //if char != space because words exist between spaces and sentence-ending puncatuation
+    {
+      textRecord.kwIndex[wordCountIndex][i] = textRecord.contents[i]; //how are we suppoesd to prase ending punctuation
+    }
 
-//for(int i = 0; i < strlen(textRecord.contents); i++) //word count logic goes in here to separate concerns
-{
- // cout << textRecord.contents[i];
-}
+    //for(int i = 0; i < wordLen; i++)
+    //{
+    //  cout << textRecord.kwIndex[wordCountIndex]
+    // }
+    // cout << textRecord.kwIndex[wordCountIndex][i];
+  }
 
-inData.close();
-return 0;
+  cout << "number of lines is " << lines << endl;
+  cout << "total chars is " << address << endl;
+  cout << "first line is x chars long " << endl;
+  //for(int i = 0; i < strlen(textRecord.contents); i++) //word count logic goes in here to separate concerns
+  //{
+  // cout << textRecord.contents[i];
+  //}
+
+  inData.close();
+  return 0;
 }
 
--- main.h
+++ main.h
@@ -9,8 +9,13 @@
 struct txtRecord
 {
   std::string userInput;
-  static const int maxLen = 1000000;
-  char contents[maxLen]; 
-  int age, charCount;
+  static const int maxLen = 10000;
+  static const int maxWords = 100;
+  static const int maxWordLen = 100;
+  static const int maxLines = 100;
+  char contents[maxLen]; //the entire text in a single c string
+  char kwIndex[maxWords][maxWordLen]; //1st 100 unique words get put into c strings here then sorted
+
+  int age, charCount; 
 };
 
